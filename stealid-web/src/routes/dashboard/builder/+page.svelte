16675b618a0ff5ef2860c10d2c2a677ace0d1266
sdgjkhdfg sdfgdfg <dfjghdjkflg@fdkgjdfg.com>
2023-05-01 10:13:44 +0530

    added builder page

--- stealid-web/src/routes/dashboard/builder/+page.svelte
+++ stealid-web/src/routes/dashboard/builder/+page.svelte
@@ -1,1 +1,14 @@
-<h1>Hello</h1>
+<script lang="ts">
+	import Box from '$lib/components/box.svelte';
+</script>
+
+<div class="primary-container">
+	<h2 class="text-3xl font-bold py-6">Builder</h2>
+	<button
+		type="submit"
+		class="bg-neutral-800 text-white rounded p-2 font-bold hover:ring ring-neutral-500 flex gap-2"
+	>
+		<Box />
+		<span>Create Build</span>
+	</button>
+</div>
